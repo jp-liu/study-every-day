b75371f47e3756a60ac2bf75f2f04db4c4d1b140
jpliu <fortune_coder@foxmail.com>
2022-04-18 21:27:06 +0800

    ✨新增(leetcode): 树
    100.相同的树

---
+++ src/leetcode/question/#100-相同的树/100.相同的树.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=typescript
+ *
+ * [100] 相同的树
+ */
+
+import { TreeNode } from '../TreeNode'
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (!p && !q) return true
+  if ((p && !q) || (!p && q)) return false
+  if (p.val !== q.val) return false
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+}
+// @lc code=end
