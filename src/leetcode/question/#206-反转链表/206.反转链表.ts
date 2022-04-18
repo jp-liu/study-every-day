7891700ad64a0cab9d65f7aca6324ada77ed1942
jpliu <fortune_coder@foxmail.com>
2022-04-18 19:05:20 +0800

    ✨新增(leetcode): 链表
    206.反转链表

---
+++ src/leetcode/question/#206-反转链表/206.反转链表.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=typescript
+ *
+ * [206] 反转链表
+ */
+
+import { ListNode } from '../ListNode'
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head) return head
+  let prev = null
+  let cur = head
+  while (cur) {
+    let next = cur.next
+    cur.next = prev
+    prev = cur
+    cur = next
+  }
+  return prev
+}
+// @lc code=end
