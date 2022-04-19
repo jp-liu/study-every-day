ee03f7cc68737e414075202518c4e050804040fd
jpliu <fortune_coder@foxmail.com>
2022-04-19 21:36:48 +0800

    ✨新增(leetcode): 二叉树
    222.完全二叉树

---
+++ src/leetcode/question/#222-完全二叉树的节点个数/222.完全二叉树的节点个数.ts
@@ -0,0 +1,34 @@
+import { TreeNode } from './../TreeNode'
+/*
+ * @lc app=leetcode.cn id=222 lang=typescript
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+  let sum = 0
+  const traverse = (root: TreeNode) => {
+    if (!root) return
+    sum++
+    traverse(root.left)
+    traverse(root.right)
+  }
+  traverse(root)
+  return sum
+}
+// @lc code=end
