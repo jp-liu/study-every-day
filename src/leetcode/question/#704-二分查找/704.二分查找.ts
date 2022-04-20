0acc7c6d748382b2b2b38e6ff2568314135a878e
jpliu <fortune_coder@foxmail.com>
2022-04-20 22:22:48 +0800

    ✨新增(leetcode): 二分
    704.二分查找

---
+++ src/leetcode/question/#704-二分查找/704.二分查找.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=typescript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+  let left = 0
+  let right = nums.length - 1
+  while (left <= right) {
+    // 避免相加越界
+    const mid = left + ((right - left) >> 1)
+    if (nums[mid] > target) {
+      right = mid - 1
+    } else if (nums[mid] < target) {
+      left = mid + 1
+    } else {
+      return mid
+    }
+  }
+  return -1
+}
+// @lc code=end
