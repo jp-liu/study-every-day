f1004b3480e2a8379aaf2833941d0ab7b64524bd
jpliu <fortune_coder@foxmail.com>
2022-05-16 22:42:07 +0800

    ✨新增(leetcode): 链表
    206.反转链表

--- src/leetcode/question/#206-反转链表/206.反转链表.ts
+++ src/leetcode/question/#206-反转链表/206.反转链表.ts
@@ -20,15 +20,20 @@ import { ListNode } from '../ListNode'
  */
 
 function reverseList(head: ListNode | null): ListNode | null {
-  if (!head) return head
-  let prev = null
-  let cur = head
-  while (cur) {
-    let next = cur.next
-    cur.next = prev
-    prev = cur
-    cur = next
-  }
-  return prev
+  // if (!head) return head
+  // let prev = null
+  // let cur = head
+  // while (cur) {
+  //   let next = cur.next
+  //   cur.next = prev
+  //   prev = cur
+  //   cur = next
+  // }
+  // return prev
+  if (!head || head.next === null) return head
+  const node = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return node
 }
 // @lc code=end
