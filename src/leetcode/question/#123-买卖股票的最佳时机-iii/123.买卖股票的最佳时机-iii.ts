70c859170e14573909d738de0649b932110ac20c
jp-liu <fortune_coder@foxmail.com>
2022-05-20 17:45:20 +0800

    ✨新增(leetcode): 动态规划
    123.买卖股票的最佳时机-Ⅲ

--- src/leetcode/question/#123-买卖股票的最佳时机-iii/123.买卖股票的最佳时机-iii.ts
+++ src/leetcode/question/#123-买卖股票的最佳时机-iii/123.买卖股票的最佳时机-iii.ts
@@ -6,6 +6,36 @@
 
 // @lc code=start
 function maxProfit(prices: number[]): number {
+  // 状态定义: dp[i][k][0] 第 i 天, 在 k 次交易下,手里不持股票最高收益
+  // 状态定义: dp[i][k][1] 第 i 天, 在 k 次交易下,手里持有股票最高收益
+  // 动态转移方程: dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]) 昨天不持/昨天持有今天卖掉
+  // 动态转移方程: dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]) 昨天持有/昨天不持有,今天买入,买入消耗一次购买机会
+  const n = prices.length
+  const K = 2 // 可交易次数
+  const dp = new Array(n + 1)
+    .fill(0)
+    .map(() => new Array(K + 1).fill(0).map(() => [0, 0]))
+  for (let k = 1; k < K + 1; k++) {
+    dp[0][k][0] = 0 // 第0天不持股
+    dp[0][k][1] = -prices[0] // 第0天持股
+  }
+  for (let i = 1; i <= n; i++) {
+    for (let k = 1; k <= K; k++) {
+      dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i - 1])
+      dp[i][k][1] = Math.max(
+        dp[i - 1][k][1],
+        dp[i - 1][k - 1][0] - prices[i - 1]
+      )
+    }
+  }
+  // 第n天,经历k次交易没手头不持有股票的收益
+  return dp[n][K][0]
+}
+// @lc code=end
+export {}
+maxProfit([3, 3, 5, 0, 0, 3, 1, 4])
+
+function maxProfit1(prices: number[]): number {
   // 最多两笔交易
   // 一次都不买入
   // 第一次买入手头最多的钱
@@ -39,5 +69,3 @@ function maxProfit(prices: number[]): number {
   }
   return dp[4]
 }
-export {}
-// @lc code=end
