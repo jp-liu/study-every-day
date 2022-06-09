cd39ec1e089182f637eec6bf21a19c6e848c909a
jpliu <fortune_coder@foxmail.com>
2022-06-09 22:04:47 +0800

    ✨新增: ✨新增(javascript): handwriting
    辅助方法

--- src/javascript/handwriting/helper/utils.js
+++ src/javascript/handwriting/helper/utils.js
@@ -3,6 +3,11 @@
  */
 const toString = value => Object.prototype.toString.call(value)
 
+/**
+ * @description 判断属性是否当前对象自身属性
+ */
+const hasOwn = (obj, key) => Object.prototype.hasOwnProperty(obj, key)
 module.exports = {
-  toString
+  toString,
+  hasOwn
 }
